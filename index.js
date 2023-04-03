d7a441178179d928a7eea489e080746108ea9d6d
lynnmoujaess <110938958+lynnmoujaess@users.noreply.github.com>
2023-04-03 23:34:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const ageSelect = document.getElementById("age");
+const option = document.createElement("option");
+option.value = "";
+option.text = "Age";
+ageSelect.appendChild(option);
+for (let i = 5; i <= 95; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.text = i;
+  ageSelect.appendChild(option);
+}
+
+function isNumberKey(evt) {
+  let charCode = evt.which;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+  }
+  return true;
+}
+  
+function checkPassword() {
+  let password = document.getElementById("password");
+  if (password.value === "quizpassword") {
+      window.open("question1.html");
+  }
+  else {
+      alert("Incorrect password.");
+      password.value="";
+  }
+}
